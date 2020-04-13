55077ac97637a79d17523193070fd2bb36378888
André Jaenisch <andre.jaenisch@posteo.de>
2020-04-13 22:16:51 +0200

    Introduce opponent

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { GameLoop, init } = require('kontra')
+const { GameLoop, init, initKeys, keyPressed } = require('kontra')
 
 const loadAssets = require('./assets')
 const renderGround = require('./scenes/game.scene')
@@ -6,21 +6,77 @@ const renderPlayer = require('./sprites/player')
 
 window.onload = async () => {
   'use strict'
-  init()
+  const { canvas } = init()
+  initKeys()
 
   const assets = await loadAssets()
   const tileEngine = renderGround(assets[0])
-  const player = renderPlayer(assets[1])
+  const player = renderPlayer(assets[1], 1)
+  const opponent = renderPlayer(assets[1], 2)
 
   tileEngine.addObject(player)
+  tileEngine.addObject(opponent)
 
   let loop = GameLoop({
     update: () => {
+      movePlayer(player)
+      moveOpponent(opponent)
     },
     render: () => {
       tileEngine.render()
       player.render()
+      opponent.render()
     }
   })
   loop.start()
+
+  function movePlayer (player) {
+    if (keyPressed('left') && player.x >= 8) {
+      player.flipped = true
+      player.turned = 0
+      player.x -= 8
+    }
+
+    if (keyPressed('right') && player.x <= (canvas.width - 21 - 8)) {
+      player.flipped = false
+      player.turned = 0
+      player.x += 8
+    }
+
+    if (keyPressed('up') && player.y >= 8) {
+      player.turned = -1
+      player.y -= 8
+    }
+
+    if (keyPressed('down') && player.y <= (canvas.height - 31 - 8)) {
+      player.turned = 1
+      player.y += 8
+    }
+    player.update()
+  }
+
+  function moveOpponent (opponent) {
+    if (keyPressed('a') && opponent.x >= 8) {
+      opponent.flipped = true
+      opponent.turned = 0
+      opponent.x -= 8
+    }
+
+    if (keyPressed('d') && opponent.x <= (canvas.width - 21 - 8)) {
+      opponent.flipped = false
+      opponent.turned = 0
+      opponent.x += 8
+    }
+
+    if (keyPressed('w') && opponent.y >= 8) {
+      opponent.turned = -1
+      opponent.y -= 8
+    }
+
+    if (keyPressed('s') && opponent.y <= (canvas.height - 31 - 8)) {
+      opponent.turned = 1
+      opponent.y += 8
+    }
+    opponent.update()
+  }
 }
--- sprites/player.js
+++ sprites/player.js
@@ -1,24 +1,93 @@
 const { Sprite, SpriteSheet } = require('kontra')
 
-function renderPlayer (image) {
+function renderPlayer (image, frame) {
   const body = SpriteSheet({
     frameWidth: 21,
     frameHeight: 31,
     image,
     animations: {
       stand: {
-        frames: 1
+        frames: frame,
+	loop: false
       }
     }
   })
 
   const sprite = Sprite({
-    x: 5 * 64,
+    x: 32 + 21 + (3 + frame) * 64,
     y: 16 + 3 * 64,
-    animations: body.animations
+    animations: body.animations,
+    flipped: false,
+    turned: 0,
+    render: function () {
+      this.context.save()
+      this.draw()
+
+      if (this.flipped) {
+	// Kudos: https://stackoverflow.com/a/3129152
+	/*
+        const image = this.currentAnimation.spriteSheet.image
+	this.context.translate(this.x, this.y)
+	this.context.scale(-1, 1)
+	console.log('Spritesheet', this.currentAnimation)
+        console.log('Flip', ...unpackDrawData(this))
+	this.context.drawImage(...unpackDrawData(this))
+	*/
+	console.log('TODO: IMPLEMENT FLIPPING')
+      }
+
+      if (this.turned === -1) {
+	/*
+        this.context.translate(this.x, this.y)
+	this.context.rotate(degreesToRadians(90))
+	this.context.drawImage(...unpackDrawData(this))
+	*/
+	console.log('TODO: IMPLEMENT TURNING UPWARDS')
+      }
+      
+      if (this.turned === 1) {
+	/*
+        this.context.translate(this.x, this.y)
+	this.context.rotate(degreesToRadians(-90))
+	this.context.drawImage(...unpackDrawData(this))
+	*/
+	console.log('TODO: IMPLEMENT TURNING DOWNWARDS')
+      }
+
+      this.context.restore()
+    }
   })
 
   return sprite
+}
+
+function unpackDrawData (sprite) {
+  const image = sprite.currentAnimation.spriteSheet.image
+  const frame = sprite.currentAnimation.frames[0]
+  const x = sprite.x
+  const y = sprite.y
+  const width = sprite.width
+  const height = sprite.height
+  const sx = frame * width
+  const sy = 0
+  const sWidth = width
+  const sHeight = height
+
+  return [
+    image,
+    sx,
+    sy,
+    sWidth,
+    sHeight,
+    x,
+    y,
+    width,
+    height
+  ]
+}
+
+function degreesToRadians (degrees) {
+  return degrees * Math.PI / 180
 }
 
 module.exports = renderPlayer
